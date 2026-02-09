021d8e9ce7041c21dee0aae640bbeb36f30d6aec
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-02-08 23:58:25 -0300

    init: primeiros exercícios com Python

---
+++ exercicio-2.py
@@ -0,0 +1,4 @@
+# Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]":
+num = input("Seja bem-vindo, digite um número: ");
+
+print(f"O número digitado foi: {num}")
---
+++ exercicio-3.py
@@ -0,0 +1,8 @@
+# Faça um programa que peça dois números e imprima a soma:
+
+num = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+soma = num + num2
+
+print(f"A soma dos números é: {soma}")
